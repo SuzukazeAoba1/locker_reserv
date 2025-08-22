661aa011ddb795958d052bdab4e54d06f02dedf4
DESKTOP-AFC1F04\USER <curljeong2000@gmail.com>
2025-08-22 17:57:43 +0900

    reservation Controller 이거 살짝 추가.(맵핑, 서비스 추가)

--- locker/src/main/java/com/globalin/locker/controller/ReservationController.java
+++ locker/src/main/java/com/globalin/locker/controller/ReservationController.java
@@ -1,7 +1,22 @@
 package com.globalin.locker.controller;
 
+import com.globalin.locker.service.AccountService;
+import com.globalin.locker.service.LockerService;
+import com.globalin.locker.service.RentalService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/reservation")
+@RequiredArgsConstructor
 public class ReservationController {
+
+    private final AccountService accountService;
+    private final LockerService lockerService;
+    private final RentalService rentalService;
+
+    
+
+
 }
