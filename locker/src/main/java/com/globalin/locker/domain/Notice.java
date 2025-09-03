f74af0711ecdea1f8793f00bf04d7868979b1aa2
Beelink_EQR6\USER <rasillrus@naver.com>
2025-09-03 17:12:31 +0900

    공지사항에 글쓴이 아이디 출력

--- locker/src/main/java/com/globalin/locker/domain/Notice.java
+++ locker/src/main/java/com/globalin/locker/domain/Notice.java
@@ -9,9 +9,10 @@ import java.sql.Timestamp;
 @AllArgsConstructor     // ✅ Lombok: 모든 필드를 매개변수로 받는 생성자 생성
 @Builder                // ✅ Lombok: 빌더 패턴 메서드 자동 생성 (객체 생성 시 가독성↑)
 public class Notice {
-    private Long id;             // 시퀀스 자동 증가 PK
-    private String title;        // 게시글 제목
-    private String content;      // 게시글 내용
-    private Long authorId;       // 작성자 ID (Accounts.id 참조)
-    private Timestamp createdAt; // 작성일시
+    private Long id;                // 시퀀스 자동 증가 PK
+    private String title;           // 게시글 제목
+    private String content;         // 게시글 내용
+    private Long authorId;          // 작성자 ID (Accounts.id 참조)
+    private Timestamp createdAt;    // 작성일시
+    private String authorUsername;
 }
